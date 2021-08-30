2226c65cd52f4023c489f1da0196c6c66311319c
edseds <edgars.cirulis@gmail.com>
2021-08-30 19:40:07 +0300

    Initial commit

---
+++ src/com/Main.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Main {
+
+    public static void method2()
+    {
+
+    }
+
+    public static void main(String[] args) {
+	   //Print out Hello World
+        System.out.println("Hello World!!!!");
+	    System.out.println("Second Hello World!");
+	 /*
+	 sadasdasdasdasd
+	 asdasdasddsasdaasd
+	 asdasdasdasdasdasd
+
+	  */
+
+    }
+}
